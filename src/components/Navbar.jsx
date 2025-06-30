81c8e5e5a115411b3a96eb737b9dfb55a4691ad7
Hesham Saif <heshamsaif856@gmail.com>
2025-06-30 19:01:49 +0300

    edit navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
     <nav
       className="navbar navbar-expand-lg custom-navbar px-4 shadow-sm sticky-top"
       data-aos="fade-down"
       data-aos-duration="500"
     >
-      <a className="navbar-brand fw-bold">
+      {/* اللوجو */}
+      <Link className="navbar-brand fw-bold" to="/">
         <div className="d-flex flex-column">
           <span style={{ color: "#00c6a7" }}>Digital Solutions</span>
           <small>مواقع - تطبيقات - دعم فني</small>
         </div>
-      </a>
+      </Link>
+
+      {/* زر التوجّل للموبايل */}
       <button
         className="navbar-toggler"
         type="button"
@@ -24,32 +30,50 @@ const Navbar = () => {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
+
       <div className="collapse navbar-collapse" id="navbarNav">
+        {/* القائمة اليمين: الرئيسية فقط */}
+        <ul className="navbar-nav me-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              الرئيسية
+            </Link>
+          </li>
+        </ul>
+
+        {/* القائمة الشمال: باقي العناصر */}
         <ul className="navbar-nav ms-auto">
-          <Link className="nav-link" to="/plans">
-            باقات الأسعار
-          </Link>
-          <Link className="nav-link" to="/faq">
-            الأسئلة الشائعة
-          </Link>
           <li className="nav-item">
-            <a className="nav-link" href="#services">
-              خدماتنا
-            </a>
+            <Link className="nav-link" to="/plans">
+              باقات الأسعار
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#contact">
-              تواصل معنا
-            </a>
+            <Link className="nav-link" to="/faq">
+              الأسئلة الشائعة
+            </Link>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#whyus">
-              ليه تختارنا ؟
-            </a>
-          </li>
-          <Link className="nav-link" to="/">
-            الرئيسية
-          </Link>
+
+          {/* عناصر تظهر فقط في الصفحة الرئيسية */}
+          {isHome && (
+            <>
+              <li className="nav-item">
+                <a className="nav-link" href="#services">
+                  خدماتنا
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">
+                  تواصل معنا
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#whyus">
+                  ليه تختارنا ؟
+                </a>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
